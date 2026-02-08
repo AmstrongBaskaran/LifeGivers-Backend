5dbe88eb373634b018d6a4d8bc1f67d731ad0cc2
AmstrongBaskaran <aamstrong522@gmail.com>
2026-02-08 13:34:47 +0530

    feat: add Pydantic schemas for platform statistics.

--- app/schemas/platform_stats_schema.py
+++ app/schemas/platform_stats_schema.py
@@ -1,13 +1,18 @@
 from pydantic import BaseModel
 
+from typing import Optional
+
 class PlatformStatsBase(BaseModel):
     total_funds_raised: str
     lives_impacted: str
     successful_campaigns: str
     success_rate: str
 
-class PlatformStatsCreate(PlatformStatsBase):
-    pass
+class PlatformStatsCreate(BaseModel):
+    lives_impacted: str
+    successful_campaigns: str
+    success_rate: str
+    total_funds_raised: Optional[str] = None
 
 class PlatformStatsResponse(PlatformStatsBase):
     id: int
